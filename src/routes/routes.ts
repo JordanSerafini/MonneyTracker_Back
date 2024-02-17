f600680e3707f0f5c4fd00b74171ffdcf90ad6b0
JordanSerafini <jordanserafini.74@gmail.com>
2024-02-17 11:57:58 +0100

    :construction: commit

---
+++ src/routes/routes.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
+
+
+// Route login
+router.post('/', )
+
+
+
+
+export default router
