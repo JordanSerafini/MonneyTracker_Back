45aaab796a8a8c10c755e8fe3c9c9a3ea7889f7b
Jordan S <jordanserafini.74@gmail.com>
2024-04-26 16:07:51 +0200

    :working:

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import controller from "../controllers/controller";
+import { pool } from "../database/pool";
 
 const router = express.Router();
 
@@ -13,10 +14,25 @@ router.post('/expense', controller.addExpense);
 router.get('/expense', controller.getAllExpense);
 router.delete('/expense/:id', controller.deleteExpense);
 
-//Route user
-router.post('/user', controller.addUser);
-router.get('/users', controller.getAllUsers);
-
+router.get('/test', async (req, res) => {
+  try {
+    const query = `
+      SELECT 
+        column_name, 
+        data_type, 
+        is_nullable 
+      FROM 
+        information_schema.columns 
+      WHERE 
+        table_name = 'expense';
+    `;  
+    const result = await pool.query(query);
+    res.json(result.rows); // Envoie le résultat au client
+  } catch (error) { 
+    console.error('Erreur lors de la récupération des colonnes:', error); // Log d'erreur pour débogage
+    res.status(500).json({ error: 'Erreur interne du serveur' }); // Réponse d'erreur avec code 500
+  }
+});
 
 
 
